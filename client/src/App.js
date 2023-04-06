2b8262c13266abc68648ec40ea6a92412bbe047b
joneam <jonathanneam@gmail.com>
2023-04-06 22:14:13 +0800

    added CRUD

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,112 @@
-import { useState } from "react";
-import "./App.css";
-import Entry from "./components/Entry";
+import { useState } from 'react';
+import './App.css';
+import Axios from 'axios';
+import Entry from './components/Entry';
 
 function App() {
-	const [studentid, setStudentid] = useState(0);
-	const [name, setName] = useState("");
-	const [schClass, setSchClass] = useState("");
-	const [grade, setGrade] = useState("");
+	const [studentNum, setStudentNum] = useState(0);
+	const [name, setName] = useState('');
+	const [studentClass, setStudentClass] = useState('');
+	const [grade, setGrade] = useState('');
 
-	const output = () => {
-		console.log(studentid + " " + name + " " + schClass + " " + grade);
+	const [studentList, setStudentList] = useState([]);
+
+	const [newGrade, setNewGrade] = useState('');
+
+	const addStudent = () => {
+		Axios.post('http://localhost:3001/create', {
+			studentNum: studentNum,
+			name: name,
+			studentClass: studentClass,
+			grade: grade,
+		}).then(() => {
+			console.log('Success!');
+			setStudentList([
+				...studentList,
+				{
+					studentNum: studentNum,
+					name: name,
+					studentClass: studentClass,
+					grade: grade,
+				},
+			]);
+		});
+	};
+
+	const getStudents = () => {
+		Axios.get('http://localhost:3001/students').then((response) => {
+			setStudentList(response.data);
+		});
+	};
+
+	const updateGrade = (id) => {
+		Axios.put('http://localhost:3001/update', {
+			grade: newGrade,
+			id: id,
+		}).then((response) => {
+			alert('updated!');
+			setStudentList(
+				studentList.map((val) => {
+					return val.id === id
+						? {
+								id: val.id,
+								studentNum: studentNum,
+								name: name,
+								studentClass: studentClass,
+								grade: newGrade,
+						  }
+						: val;
+				})
+			);
+		});
+	};
+
+	const deleteStudent = (id) => {
+		Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+			setStudentList(
+				studentList.filter((val) => {
+					return val.id !== id;
+				})
+			);
+		});
 	};
 
 	return (
 		<div className='App'>
 			<h1>Student Grade Tracker!</h1>
+			{/* ----Static HTML---- */}
+			{/* <label className='entryComponent'>Student Number: </label>
+			<input
+				className='entryInput'
+				onChange={(e) => {
+					setStudentNum(e.target.value);
+				}}
+			></input>
+			<label className='entryComponent'>Name: </label>
+			<input
+				className='entryInput'
+				onChange={(e) => {
+					setName(e.target.value);
+				}}
+			/>
+			<label className='entryComponent'>Class: </label>
+			<input
+				className='entryInput'
+				onChange={(e) => {
+					setStudentClass(e.target.value);
+				}}
+			></input>
+			<label className='entryComponent'>Grade: </label>
+			<input
+				className='entryInput'
+				onChange={(e) => {
+					setGrade(e.target.value);
+				}}
+			></input> */}
+			{/* ----Components---- */}
 			<Entry
-				text='Student ID'
-				stateText={setStudentid}
+				text='Student Number'
+				stateText={setStudentNum}
 			/>
 			<Entry
 				text='Name'
@@ -25,7 +114,7 @@ function App() {
 			/>
 			<Entry
 				text='Class'
-				stateText={setSchClass}
+				stateText={setStudentClass}
 			/>
 			<Entry
 				text='Grade'
@@ -33,10 +122,52 @@ function App() {
 			/>
 			<button
 				className='button'
-				onClick={output}
+				onClick={addStudent}
 			>
 				Add Student
 			</button>
+			<br />
+			<hr />
+			<button
+				className='button'
+				onClick={getStudents}
+			>
+				Show Students
+			</button>
+			{studentList.map((val, key) => {
+				return (
+					<div className='studentList'>
+						<div>
+							<h3>{val.studentNum}</h3>
+							<h3>{val.name}</h3>
+							<h3>{val.studentClass}</h3>
+							<h3>{val.grade}</h3>
+						</div>
+						<div>
+							<input
+								placeholder='enter amount'
+								onChange={(e) => {
+									setNewGrade(e.target.value);
+								}}
+							/>
+							<button
+								onClick={() => {
+									updateGrade(val.id);
+								}}
+							>
+								Update Grade
+							</button>
+							<button
+								onClick={() => {
+									deleteStudent(val.id);
+								}}
+							>
+								Delete
+							</button>
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,73 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+	user: 'root',
+	host: 'localhost',
+	password: 'ne@m1234',
+	database: 'student_grade',
+});
+
+app.post('/create', (req, res) => {
+	const studentNum = req.body.studentNum;
+	const name = req.body.name;
+	const studentClass = req.body.studentClass;
+	const grade = req.body.grade;
+
+	db.query(
+		'INSERT INTO students (student_number, name, class, grade) VALUES (?,?,?,?)',
+		[studentNum, name, studentClass, grade],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send('Entry inserted!');
+			}
+		}
+	);
+});
+
+app.get('/students', (req, res) => {
+	db.query('SELECT * FROM students', (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	});
+});
+
+app.put('/update', (req, res) => {
+	const id = req.body.id;
+	db.query(
+		'UPDATE students SET grade = ? WHERE id = ?',
+		[grade, id],
+		(err, result) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send(result);
+			}
+		}
+	);
+});
+
+app.delete('/delete/:id', (req, res) => {
+	const id = req.params.id;
+	db.query('DELETE FROM students WHERE id = ?', id, (err, result) => {
+		if (err) {
+			console.log('err');
+		} else {
+			res.send(result);
+		}
+	});
+});
 
 app.listen(3001, () => {
-	console.log("server is running on port 3001!");
+	console.log('server is running on port 3001!');
 });
